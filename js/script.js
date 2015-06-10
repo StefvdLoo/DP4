d66cb8bd6f127b76772a1f0af0484e867ed916ba
StefvdLoo <stefvanderloo92@gmail.com>
2015-06-10 14:45:07 +0200

    Platform deel Stef

--- js/script.js
+++ js/script.js
@@ -1,10 +1,54 @@
 // external js: masonry.pkgd.js
 
 $(document).ready( function() {
-
-  $('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: 120
-  });
-  
+	
 });
+
+$(window).resize(function(){
+
+       if ($(window).width() <= 400) {  
+	       $('.grid').masonry({
+			    itemSelector: '.grid-item',
+			    columnWidth: 80,
+			    isOriginTop: false
+			});
+              // is mobile device
+       }
+
+       if ($(window).width() <= 550) {  
+	       $('.grid').masonry({
+			    itemSelector: '.grid-item',
+			    columnWidth: 90,
+			    isOriginTop: false
+			});
+              // is mobile device
+       }
+
+       if ($(window).width() <= 750) {  
+	       $('.grid').masonry({
+			    itemSelector: '.grid-item',
+			    columnWidth: 100,
+			    isOriginTop: false
+			});
+              // is mobile device
+       }
+
+       if ($(window).width() <= 1000) {  
+	       $('.grid').masonry({
+			    itemSelector: '.grid-item',
+			    columnWidth: 320,
+			    isOriginTop: false
+			});
+              // is mobile device
+       }
+
+       if ($(window).width() >= 1001) {  
+	       $('.grid').masonry({
+			    itemSelector: '.grid-item',
+			    columnWidth: 350,
+			    isOriginTop: false
+			});
+              // is mobile device
+       }         
+
+	});
